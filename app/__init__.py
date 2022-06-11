5538655f6d6119585af2fdff4a181cb2b52f5bdb
gansior <breslau1962@yandex.ru>
2022-06-11 08:57:23 +0300

    add new modules

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,7 @@ app = Flask(__name__,
             static_folder='static',
             template_folder='templates')
 
+tsession={"regim":'0',
+          "rul":'0'}
+
 from app.controllers import rootFirst
--- app/controllers/rootFirst.py
+++ app/controllers/rootFirst.py
@@ -1,8 +1,9 @@
 from flask import render_template
 from app import app
-
+from app import tsession
 
 
 @app.route('/')
+@app.route('/StartPage/')
 def startPage():
-    return render_template('index.html')
+    return render_template('index.html', code=tsession["regim"])
